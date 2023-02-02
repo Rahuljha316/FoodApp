c1176c6d7d318da63919d60224aa064f88304018
Rahul Jha <rahulbudy11@gmail.com>
2023-02-02 22:40:55 +0530

    added filter functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,70 @@
-import { restuarentlist } from '../config';
-import RestuarentCard from './RestuarentCard'
+import { restuarentlist } from "../config";
+import RestuarentCard from "./RestuarentCard";
+import { useState } from "react";
 
-const Body = ()=>{
-    return (
-        <div className="restaurent-list">
-            {/* {RestuarentCard(restuarentlist[0])} */}
-            {
-                restuarentlist.map(
-                    (restaurent) => {
-                        return <RestuarentCard {...restaurent.data.data} key={restaurent.data.data.id}/>
-                    }
-                )
-            }
-            {/* <RestuarentCard {...restuarentlist[0].data.data}/>
+const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  // const [searchClicked, setSearchClicked] = useState();
+  const [restaurent, setRestaurent] = useState(restuarentlist);
+
+  const filterData = (searchText, restaurent) => {
+    const filteredData =restaurent.filter((restaurent) =>
+      restaurent.data.data.name.includes(searchText)
+    );
+    return filteredData
+  };
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        {/* <h1>{searchClicked}</h1> */}
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, restaurent);
+            setRestaurent(data)
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurent-list">
+        {restaurent.map((restaurent) => {
+          return (
+            <RestuarentCard
+              {...restaurent.data.data}
+              key={restaurent.data.data.id}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
+
+{
+  /* {RestuarentCard(restuarentlist[0])} */
+}
+
+{
+  /* <RestuarentCard {...restuarentlist[0].data.data}/>
             <RestuarentCard {...restuarentlist[1].data.data}/>
             <RestuarentCard {...restuarentlist[2].data.data}/>
             <RestuarentCard {...restuarentlist[3].data.data}/>
-            <RestuarentCard {...restuarentlist[4].data.data}/> */}
-            {/* <RestuarentCard restuarent={restuarentlist[5]}/>
-            <RestuarentCard restuarent={restuarentlist[6]}/> */}
-            
-
-        </div>
-    )
+            <RestuarentCard {...restuarentlist[4].data.data}/> */
 }
-
-export default Body;
+{
+  /* <RestuarentCard restuarent={restuarentlist[5]}/>
+            <RestuarentCard restuarent={restuarentlist[6]}/> */
+}
--- src/components/RestuarentCard.js
+++ src/components/RestuarentCard.js
@@ -1,33 +1,37 @@
-import {IMG_CDN_URL} from '../config'
+import { IMG_CDN_URL } from "../config";
 
+const RestuarentCard = ({
+  name,
+  cloudinaryImageId,
+  lastMileDistance,
+  cuisines,
+}) => {
+  // console.log(props.restuarent.data.data?.name)
+  // const {data:{
+  //     data:{name,
+  //     cuisines,
+  //     cloudinaryImageId,lastMileDistance
 
-const RestuarentCard = ({ name,cloudinaryImageId,lastMileDistance,cuisines }) =>{
-    // console.log(props.restuarent.data.data?.name)
-    // const {data:{
-    //     data:{name,
-    //     cuisines,
-    //     cloudinaryImageId,lastMileDistance
-        
-    //     }
-    // }
-    
-    // } = restuarent;
-    // const [name,cloudinaryImageId,lastMileDistance,cuisines] = restuarent.data
-    
-   console.log(name);
-    return (
-        <div className="card">
-            <img src={IMG_CDN_URL + cloudinaryImageId}></img>
-            {/* <h2>{props.restuarent.data.data?.name}</h2> */}
-            <h2>{name}</h2>
-            <h3>{cuisines.join(' ,')}</h3>
-            <h4>{lastMileDistance}</h4>
-            {/* <h2>{props.restuarent.data.data?.cuisines.join(' , ')}</h2>
+  //     }
+  // }
+
+  // } = restuarent;
+  // const [name,cloudinaryImageId,lastMileDistance,cuisines] = restuarent.data
+
+  console.log(name);
+  return (
+    <div className="card">
+      <img src={IMG_CDN_URL + cloudinaryImageId}></img>
+      {/* <h2>{props.restuarent.data.data?.name}</h2> */}
+      <h2>{name}</h2>
+      <h3>{cuisines.join(" ,")}</h3>
+      <h4>{lastMileDistance}</h4>
+      {/* <h2>{props.restuarent.data.data?.cuisines.join(' , ')}</h2>
             <h3>{props.restuarent.data.data?.lastMileTravel} Minutes</h3> */}
-            {/* <h3>Burger ,American</h3>
+      {/* <h3>Burger ,American</h3>
             <h2>Burger ,American</h2> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default RestuarentCard;
